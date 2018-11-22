181575b17c3b335af97512d5c335bd93e046c5b6
yamauchi.ryoji <yamauchi.ryoji@m-field.co.jp>
2018-11-22 18:25:26 +0900

    関数として切り出し

--- main.js
+++ main.js
@@ -4,18 +4,32 @@ document.getElementById('form').addEventListener('submit', function(event) {
   var userName = document.getElementById('user-name').value
   var email = document.getElementById('email').value
 
-  //ユーザー名がからっぽなら、エラーメッセージを出す
-  if(userName === '') {
+  if(isEmpty(userName)) {
     document.getElementById('user-name-error').innerHTML = 'ユーザーネームを入力してください'
+    event.preventDefault()
   }
 
-  //Eメールが空っぽ
-  if(email === '') {
+  if(isEmpty(email)) {
     document.getElementById('email-error').innerHTML = 'Eメールアドレスを入力してください'
+    event.preventDefault()
   }
 
-  //Eメールの形式が不正
-  if(!email.match( /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+  if(!isEmail(email)) {
     document.getElementById('email-error').innerHTML = 'Eメールアドレスの形式がおかしいです'
+    event.preventDefault()
   }
+
 })
+
+// Eメールアドレスの形式チェック
+function isEmail(value) {
+  if(value.match( /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+    return true
+  }
+  return false
+}
+
+// 空っぽかどうか
+function isEmpty(value) {
+  return value === ''
+}
