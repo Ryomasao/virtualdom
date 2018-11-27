3f5c0ba5e65bd260c65532a92b586037ff6ede9d
yamauchi.ryoji <yamauchi.ryoji@m-field.co.jp>
2018-11-27 16:03:42 +0900

    webpackを使う

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-//import Hello from './hello.js';
+import { isEmpty, isEmail } from './validate.js'
 
-//const items = [1, 2, 3]
-//
-//for(const item of items) {
-//  console.log(item)
-//}
-//
-//console.log('test')
+//formのsubmitボタンを押したときのイベント
+document.getElementById('form').addEventListener('submit', function(event) {
+  //
+  var userName = document.getElementById('user-name').value
+  var email = document.getElementById('email').value
 
-//const myHello =  new Hello
-//myHello.sayHello()
-//console.log('main')
+  if(isEmpty(userName)) {
+    document.getElementById('user-name-error').innerHTML = 'ユーザーネームを入力してください'
+    event.preventDefault()
+  }
+
+  if(isEmpty(email)) {
+    document.getElementById('email-error').innerHTML = 'Eメールアドレスを入力してください'
+    event.preventDefault()
+  }
+
+  if(!isEmail(email)) {
+    document.getElementById('email-error').innerHTML = 'Eメールアドレスの形式がおかしいです'
+    event.preventDefault()
+  }
+
+})
+
